792eabb7eb28a08bd50d39881b9d3881cdd92608
chenmh <chenmh@sucsoft.com>
2022-07-21 13:47:15 +0800

    chore(init): init config

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
-import { createApp } from 'vue'
-import { Button, Toast } from '@nutui/nutui-taro';
+import "@nutui/nutui-taro/dist/style.css";
+import { createApp } from "vue";
+import { Icon, Toast } from "@nutui/nutui-taro";
 
-import './app.scss'
+import "./app.scss";
 
 const App = createApp({
-  onShow (options) {},
-  // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
+    onShow() {
+    },
+    // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
 })
 
-App.use(Button).use(Toast)
+App.use(Toast).use(Icon)
 
 export default App

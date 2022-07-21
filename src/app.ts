64bd195baa9a98309c8e9a76764670ab758c7782
lopo <lopo@zju.edu.cn>
2022-07-21 22:34:01 +0800

    feat: calendar need popup

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import "@nutui/nutui-taro/dist/style.css";
 import { createApp } from "vue";
-import { Icon, Toast } from "@nutui/nutui-taro";
+import { Icon, Popup, Toast } from "@nutui/nutui-taro";
 
 import "./app.scss";
 
@@ -10,6 +10,8 @@ const App = createApp({
     // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
 })
 
-App.use(Toast).use(Icon)
+App.use(Icon)
+    .use(Popup)
+    .use(Toast)
 
 export default App

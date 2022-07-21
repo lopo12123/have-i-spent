22469c0ecf2104b3a8f321e6ed53ee836b525245
lopo <lopo@zju.edu.cn>
2022-07-21 15:45:15 +0800

    feat: output file

---
+++ src/pages/output/output.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <view class="output-page">
+        导出
+    </view>
+</template>
+
+<style lang="scss" scoped>
+.output-page {
+    position: relative;
+    width: 100%;
+    height: 100%;
+}
+</style>

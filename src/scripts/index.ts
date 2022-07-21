ee3f532e9d1dd8ca905d562ef1f9e0bd806a41af
lopo <lopo@zju.edu.cn>
2022-07-21 15:54:42 +0800

    feat: pages and router

---
+++ src/scripts/index.ts
@@ -0,0 +1,14 @@
+import { navigateTo } from "@tarojs/taro";
+
+export type ValidPageName = 'index' | 'output'
+
+const Mapper: { [k in ValidPageName]: string } = {
+    index: 'pages/index/index',
+    output: 'pages/output/output',
+}
+
+export const jumpTo = (pageName: ValidPageName) => {
+    return navigateTo({
+        url: Mapper[pageName]
+    })
+}

2a9cbbc5223c3666c8f28e617626da10e58cc4e0
lopo <lopo@zju.edu.cn>
2022-07-21 22:34:09 +0800

    feat: calendar

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -3,15 +3,37 @@ import { ref } from "vue";
 import { Calendar } from "@nutui/nutui-taro";
 import { DayRecord } from "../../scripts/store";
 import StorageInfo from "../../components/StorageInfo.vue";
+import { showLoading } from "@tarojs/taro";
 
+const weekDays = [ '星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六' ]
+
+// 日历可见性
 const calendarVisible = ref(false)
+// 日历绑定日期
 const showDate = ref(new Date().toLocaleDateString().replace(/\//g, '-'))
+// 展示星期几
+const showDay = ref(weekDays[new Date().getDay()])
+// 点击日历回调 dateArray: [year, month, date, yyyy-mm-dd, day]
 const setShowDate = (dateArray: string[]) => {
-    showDate.value = dateArray[3]
     console.log(dateArray)
+
+    showDate.value = dateArray[3]
+    showDay.value = dateArray[4]
+    syncRecords(dateArray[0].toString() + parseInt(dateArray[1]), parseInt(dateArray[2]) + '')
 }
 
 const records = ref<DayRecord>([])
+const recordState = ref<'free' | 'block'>('free')
+const syncRecords = (ym: string, day: string) => {
+    showLoading({
+        title: `查询中`,
+        mask: true,
+        complete: () => {
+
+            console.log('展示: ', ym, day)
+        }
+    })
+}
 </script>
 
 <template>
@@ -19,15 +41,15 @@ const records = ref<DayRecord>([])
         <StorageInfo/>
 
         <view class="date-picker" @tap="calendarVisible = true">
-            <text class="label">当前</text>
-            <text class="value">{{ showDate }}</text>
+            <text class="value">{{ showDate }} / {{ showDay }}</text>
+            <text class="label">点击切换日期</text>
         </view>
 
         <view class="record-list">
             {{ records }}
         </view>
 
-        <Calendar title="选择日期查看明细"
+        <Calendar title="选择日期"
                   v-model:visible="calendarVisible"
                   :is-auto-back-fill="true"
                   :default-value="showDate"
@@ -48,16 +70,26 @@ const records = ref<DayRecord>([])
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: flex-start;
+    justify-content: space-evenly;
 
     .date-picker {
         position: relative;
-        width: 100%;
+        width: calc(100% - 1.5rem);
         height: 3rem;
+        padding: 0 0.75rem;
+        border-radius: 0.5rem;
         box-shadow: 5px 5px 5px #ddd;
         display: flex;
         align-items: center;
         justify-content: space-between;
+
+        .label {
+            color: #777;
+        }
+
+        .value {
+            color: #aaa;
+        }
     }
 
     .record-list {

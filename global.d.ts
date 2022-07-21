76a677cbbff1c46ce8ef6291aa5127d1f69da654
lopo <lopo@zju.edu.cn>
2022-07-21 15:45:07 +0800

    feat: .d.ts

--- global.d.ts
+++ global.d.ts
@@ -12,7 +12,14 @@ declare module '*.sass';
 declare module '*.styl';
 
 declare namespace NodeJS {
-  interface ProcessEnv {
-    TARO_ENV: 'weapp' | 'swan' | 'alipay' | 'h5' | 'rn' | 'tt' | 'quickapp' | 'qq' | 'jd'
-  }
+    interface ProcessEnv {
+        TARO_ENV: 'weapp' | 'swan' | 'alipay' | 'h5' | 'rn' | 'tt' | 'quickapp' | 'qq' | 'jd'
+    }
+}
+
+declare module '*.vue' {
+    import type { DefineComponent } from 'vue'
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+    const component: DefineComponent<{}, {}, any>
+    export default component
 }

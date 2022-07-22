402f0b10440a9f4e9de67a1624484d7a012b93aa
lopo <lopo@zju.edu.cn>
2022-07-22 16:09:17 +0800

    style: title label

--- src/components/StorageInfo.vue
+++ src/components/StorageInfo.vue
@@ -82,7 +82,7 @@ onMounted(() => {
             <NutButton size="mini" @tap="syncStorageInfo(true)">
                 {{ detectState === 'free' ? '重新检测' : '检测中 . .' }}
             </NutButton>
-            <NutButton size="mini" @tap="jumpTo('output')">使用导出</NutButton>
+            <NutButton size="mini" @tap="jumpTo('output')">日志管理</NutButton>
         </view>
     </view>
 </template>
